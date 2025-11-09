cd893ab0cac58143d6ec893f916d5d1a1261186c
octanevz <octanevz@gmx.net>
2025-11-09 03:05:38 +0100

    Add setup scripts for Ubuntu 24.04

---
+++ linux/ubuntu/24.04/setup_1_packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e;
+
+# Update OS and install some required packages
+sudo apt update -y \
+  && sudo apt upgrade -y \
+  && sudo apt install -y \
+      btop \
+      ca-certificates \
+      curl \
+      gimp \
+      git \
+      gnome-keyring \
+      gnome-shell-extensions \
+      gnome-tweaks \
+      gpg \
+      libc6 \
+      libfontconfig1 \
+      libfuse2 \
+      libgcc-s1 \
+      libicu-dev \
+      libicu74 \
+      libjpeg-turbo8 \
+      liblttng-ust1 \
+      libsecret-1-0 \
+      libsecret-1-dev \
+      libssl3 \
+      libstdc++6 \
+      lsb-release \
+      unzip \
+      wget \
+      zlib1g \
+      zsh \
+      zsh-autosuggestions \
+      zsh-syntax-highlighting \
+  && sudo apt autoremove -y \
+  && sudo apt clean -y;
+
+# Install/Update Snap Packages
+sudo snap refresh;
+
+# Reboot OS
+sudo reboot now;
---
+++ linux/ubuntu/24.04/setup_4_fonts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e;
+
+# -------------------------------------
+# Powerline Fonts and TTF MS Core Fonts
+# -------------------------------------
+sudo apt update -y \
+  && sudo apt upgrade -y \
+  && sudo apt install -y \
+      fonts-powerline \
+      ttf-mscorefonts-installer \
+  && sudo apt autoremove -y \
+  && sudo apt clean -y;
+
+# -------------------
+# JetBrains Mono Font
+# -------------------
+
+# Clean up any previous downloads
+rm -rf /tmp/JetBrainsMono-2.304.zip /tmp/JetBrainsMono;
+sudo rm -rf /usr/share/fonts/truetype/jetbrains-mono;
+
+# Download and unzip the font file
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip -O /tmp/JetBrainsMono-2.304.zip;
+unzip /tmp/JetBrainsMono-2.304.zip -d /tmp/JetBrainsMono;
+
+# Move font files to system fonts directory and update font cache
+sudo mkdir -p /usr/share/fonts/truetype/jetbrains-mono;
+sudo mv /tmp/JetBrainsMono/fonts/ttf /usr/share/fonts/truetype/jetbrains-mono;
+sudo fc-cache -f -v;
+
+# Clean up
+rm -rf /tmp/JetBrainsMono-2.304.zip /tmp/JetBrainsMono;
